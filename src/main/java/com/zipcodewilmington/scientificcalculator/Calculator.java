b7e727fa12a59d7f56dc270ef2479a20639cfe9e
payel <payel.nandy2580@gmail.com>
2022-06-25 11:52:48 -0400

    Calculator (+,-, *,/, error)

--- src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
@@ -5,27 +5,57 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int x;
+        double x;
         x = 0;
         System.out.println(0);
         System.out.print("Enter the number");
-        x=input.nextInt();
+        x=input.nextDouble();
         System.out.println(x);
-        int y;
-        y = input.nextInt();
+        double y;
+        y = input.nextDouble();
         System.out.println(y);
-        System.out.println("Enter operator: Addition, Substraction, Multiplication and Division");
+        System.out.println("Enter operator: Addition, Subtraction, Multiplication and Division");
         String Op = input.next();
         System.out.println(Op);
         if ( Op.equals("Addition")) {
-            int n;
+            double n;
             n = x + y;
             System.out.println(n);
+        } else if (Op.equals("Subtraction")) {
+            double n;
+            n = x - y;
+            System.out.println(n);
+        } else if (Op.equals("Multiplication")) {
+            double n;
+            n = x * y;
+            System.out.println(n);
+        } else if
+        (Op.equals("Division")) {
+            if (y != 0) {
+                double n;
+                n = x / y;
+                System.out.println(n);
+            } else {
+                System.out.println("Error");
+            }
+
         }
+            else {System.out.println("This is not a required function.");
+
+            }
+
+
+
+
+        }
+
 
     }
 
 
-}
+
+
+
+
 
 
